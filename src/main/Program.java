1ea5669a6e79a7e0fb2d01a7a8e8f6dd8b1a78b7
markinog <marcussla9@gmail.com>
2025-03-07 19:21:24 -0300

    testando conexao com github

--- src/main/Program.java
+++ src/main/Program.java
@@ -3,7 +3,7 @@ package main;
 public class Program {
 
     public static void main(String[] args) {
-        System.out.println("oooi");
+        System.out.println("testando pra ver se está conectado ao github");
     }
 
 }
