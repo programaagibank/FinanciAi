118bcd1966e9edf49887c1897345e7a285622dcc
rafael.garcia <Rafael.garcia21@outlook.com>
2025-03-12 11:11:01 -0300

    classe conection criada

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -1,7 +1,14 @@
 package dao;
 
+import model.entities.Cliente;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class ClienteDAO {
 
-    private String name;
 
 }
