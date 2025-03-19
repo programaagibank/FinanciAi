67df76ef745928df5d7307a26c4acf9f4e398f89
vinicius.cabral <vinicius.c.lima2004@gmail.com>
2025-03-19 11:51:03 -0300

    criação tipo SAC

--- src/com/financiai/controller/FinanciamentoController.java
+++ src/com/financiai/controller/FinanciamentoController.java
@@ -2,16 +2,19 @@ package com.financiai.controller;
 
 import com.financiai.model.enums.TipoAmortizacao;
 import com.financiai.services.Price;
+import com.financiai.services.SAC;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FinanciamentoController {
 
 
 
-    public static void calcularFinanciamento(double rendaMensal, double valorImovel, double valorEntrada, double taxaJurosAnual, int prazo, TipoAmortizacao amortizacao) {
+    public static void calcularFinanciamento(double rendaMensal, double valorImovel, double valorEntrada,
+                                             double taxaJurosAnual, int prazo, TipoAmortizacao amortizacao) {
         double valorFinanciamento = valorImovel - valorEntrada;
-        double taxaJurosMensal = taxaJurosAnual / 12 / 100;
+        double taxaJurosMensal = taxaJurosAnual / 12 / 100 ;
         double limiteParcela = rendaMensal * 0.3;
 
         if(amortizacao == TipoAmortizacao.PRICE){
@@ -35,6 +38,11 @@ public class FinanciamentoController {
                 double juros = parcelas.get(i) - amortizacoes.get(i);
                 System.out.printf("%d | %.2f | %.2f | %.2f\n", i + 1, parcelas.get(i), amortizacoes.get(i), juros);
             }
+        }
+        if (amortizacao == TipoAmortizacao.SAC){
+            SAC sac = new SAC();
+            List<Double> parcelasSac = sac.calculaParcela(valorFinanciamento, taxaJurosMensal, prazo);
+            List<Double> amortizacaoSac = sac.calculaAmortizacao(valorFinanciamento, taxaJurosMensal, prazo);
         }
     }
 
--- src/com/financiai/services/SAC.java
+++ src/com/financiai/services/SAC.java
@@ -1,4 +1,32 @@
 package com.financiai.services;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SAC {
+public class SAC implements Amortizacao{
+    @Override
+    public List<Double> calculaParcela(Double valorFinanciamento, Double taxaJuros, int prazo) {
+        List<Double> parcelaSAC = new ArrayList<>();
+        double saldoDevedor = valorFinanciamento;
+        double valorAmortizacao = valorFinanciamento / prazo;
+        for (int i = 0; i < prazo; i++) {
+
+            double parcela = valorAmortizacao + (saldoDevedor * taxaJuros);
+            parcelaSAC.add(parcela);
+
+            saldoDevedor -= parcela + valorAmortizacao;
+
+        }
+        return parcelaSAC;
+    }
+
+    @Override
+    public List<Double> calculaAmortizacao(Double valorFinanciamento, Double taxaJuros, int prazo) {
+       List<Double> amortizacao = new ArrayList<>();
+       double valorAmortizacao = valorFinanciamento / prazo;
+
+        for (int i = 0; i < prazo; i++) {
+            amortizacao.add(valorAmortizacao);
+        }
+        return amortizacao;
+    }
 }
