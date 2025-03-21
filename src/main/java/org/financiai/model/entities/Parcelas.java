8a050e56e08a21bd8d098abf976e67a97b1bddc1
RAFAEL GARCIA <Rafael.garcia21@outlook.com>
2025-03-21 13:18:03 -0300

    private int id; // ID da parcela  private int financiamentoId; // ID do financiamento associado à parcela  // Construtor com todos os atributos (incluindo id)    public Parcelas(int id, int numeroParcela, Double valorParcela, Double valorAmortizacao, int financiamentoId) {  this.id = id;// ID da parcela  this.financiamentoId = financiamentoId;// ID do financiamento associado à parcela
     / Getters e Setters
    
         public int getId() {// ID da parcela
             return id;
         }
    
         public void setId(int id) {// ID da parcela
             this.id = id;
         }
    
          public int getFinanciamentoId() {// ID do financiamento associado à parcela
                 return financiamentoId;
             }
    
             public void setFinanciamentoId(int financiamentoId) {// ID do financiamento associado à parcela
                 this.financiamentoId = financiamentoId;
             }

--- src/main/java/org/financiai/model/entities/Parcelas.java
+++ src/main/java/org/financiai/model/entities/Parcelas.java
@@ -2,15 +2,32 @@ package org.financiai.model.entities;
 
 public class Parcelas {
 
+
+    private int id; // ID da parcela
     private int numeroParcela;
     private Double valorParcela;
     private Double valorAmortizacao;
+    private int financiamentoId; // ID do financiamento associado à parcela
 
-    public Parcelas(int numeroParcela, Double valorParcela) {
+
+    // Construtor com todos os atributos (incluindo id)
+    public Parcelas(int id, int numeroParcela, Double valorParcela, Double valorAmortizacao, int financiamentoId) {
+        this.id = id;// ID da parcela
         this.numeroParcela = numeroParcela;
         this.valorParcela = valorParcela;
+        this.valorAmortizacao = valorAmortizacao;
+        this.financiamentoId = financiamentoId;// ID do financiamento associado à parcela
     }
 
+    // Getters e Setters
+
+    public int getId() {// ID da parcela
+        return id;
+    }
+
+    public void setId(int id) {// ID da parcela
+        this.id = id;
+    }
 
     public int getNumeroParcela() {
         return numeroParcela;
@@ -28,5 +45,35 @@ public class Parcelas {
         this.valorParcela = valorParcela;
     }
 
+    public Double getValorAmortizacao() {
+        return valorAmortizacao;
+    }
+
+    public void setValorAmortizacao(Double valorAmortizacao) {
+        this.valorAmortizacao = valorAmortizacao;
+    }
+
+    public int getFinanciamentoId() {// ID do financiamento associado à parcela
+        return financiamentoId;
+    }
+
+    public void setFinanciamentoId(int financiamentoId) {// ID do financiamento associado à parcela
+        this.financiamentoId = financiamentoId;
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
