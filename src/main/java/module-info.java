954ece9604517f60374aa95c1dd94357661b49a3
RAFAEL GARCIA <Rafael.garcia21@outlook.com>
2025-04-03 18:18:02 -0300

    fg

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,9 @@ module financiai.financiai {
     requires itextpdf;
     requires java.sql;
     requires java.desktop;
+    requires org.junit.jupiter.api;
+    requires org.mockito;
+    requires org.testfx.junit5;
 
     opens financiai.financiai to javafx.fxml;
     exports financiai.financiai;
